8d1ed72e1c191dd97bcf1094a6cc1fb807a2eab8
Viet Phan <vietphan90vn@gmail.com>
2020-03-12 15:43:31 +0700

    Update project structure

---
+++ ShaderGraph_Training/Assets/ShaderGraph_ExampleLibrary/Scripts/ToonShaderLightSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ToonShaderLightSettings : MonoBehaviour
+{
+	private Light _light;
+
+	void OnEnable()
+	{
+		_light = GetComponent<Light>();
+	}
+
+	void Update ()
+	{
+		Shader.SetGlobalVector("_ToonLightDirection", -transform.forward);
+		Shader.SetGlobalColor("_ToonLightColor", _light.color);
+	}
+}
